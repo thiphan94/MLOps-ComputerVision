6153a9162190884015b982d0c191eae64ade49bb
Thi Phan <thiphan94@gmail.com>
2023-10-13 16:36:09 +0200

    update

---
+++ main.py
@@ -0,0 +1,9 @@
+from cnnClassifier import logger
+
+
+# from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+# from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+# from cnnClassifier.pipeline.stage_03_training import ModelTrainingPipeline
+# from cnnClassifier.pipeline.stage_04_evaluation import EvaluationPipeline
+
+logger.infor("Welcome")
