862745766bf789b022d2ee4690a544e8dd9a9d16
Thi Phan <thiphan94@gmail.com>
2023-10-18 23:33:27 +0200

    add f1 metric

--- src/cnnClassifier/components/evaluation.py
+++ src/cnnClassifier/components/evaluation.py
@@ -2,6 +2,8 @@ import tensorflow as tf
 from pathlib import Path
 from cnnClassifier.entity.config_entity import EvaluationConfig
 from cnnClassifier.utils.common import save_json
+from sklearn.metrics import f1_score
+import numpy as np
 
 
 class Evaluation:
@@ -25,7 +27,7 @@ class Evaluation:
             directory=self.config.training_data,
             subset="validation",
             shuffle=False,
-            **dataflow_kwargs
+            **dataflow_kwargs,
         )
 
     @staticmethod
@@ -37,6 +39,25 @@ class Evaluation:
         self._valid_generator()
         self.score = model.evaluate(self.valid_generator)
 
+        predictions = model.predict(self.valid_generator)
+        y_pred = np.argmax(predictions, axis=1)
+
+        # with open("predictions.txt", "w") as f:
+        #     for pred in predictions:
+        #         f.write(f"{pred}\n")
+
+        # with open("y_pred.txt", "w") as f:
+        #     for pred in y_pred:
+        #         f.write(f"{pred}\n")
+
+        true_labels = self.valid_generator.labels
+
+        # with open("true_labels.txt", "w") as f:
+        #     for label in true_labels:
+        #         f.write(f"{label}\n")
+
+        self.f1 = f1_score(true_labels, y_pred)
+
     def save_score(self):
-        scores = {"loss": self.score[0], "accuracy": self.score[1]}
+        scores = {"loss": self.score[0], "accuracy": self.score[1], "f1_score": self.f1}
         save_json(path=Path("scores.json"), data=scores)
